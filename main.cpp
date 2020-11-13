4fedee0c20623cc7df7159f176ceae5bae9be3e2
MaximTiberiu <maxim.tiberiu.info@gmail.com>
2020-11-13 14:58:55 +0200

    update signup design

--- main.cpp
+++ main.cpp
@@ -3,17 +3,7 @@
 #include "include/Interface.h"
 
 int main() {
-    std::string testFile = "testFile1.txt";
-
     Interface i;
-
-    std::ifstream fin;
-    std::ofstream fout;
-
-    /*Interface::openFile(fin, testFile);
-    i.startAppTEST(fin);
-    Interface::closeFile(fin);*/
-
     i.startApp();
 
     std::cin.get();
